30c6801921bcb19b88bb374e41091f6b89448762
LiamKirkland <liam.b.kirkland@gmail.com>
2026-09-02 12:48:37 -0400

    Add ability to submit review, reviews shown on game page

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -2,5 +2,5 @@ class UserGame < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  validates :score, numericality: { in: 0..10 }
+  validates :score, numericality: { in: 0..10 }, allow_nil: true
 end
